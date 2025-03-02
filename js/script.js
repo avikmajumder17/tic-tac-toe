109c251bc00d2fb6b9baca722c2b6b71a64423f3
Avik Majumder <avikmajumder17@gmail.com>
2025-03-02 23:48:12 +0530

    Updated gameover design

--- js/script.js
+++ js/script.js
@@ -20,25 +20,27 @@ const checkWinner = () => {
     let cellText = document.getElementsByClassName("cell-text");
 
     let wins = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, 5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135]
     ]
 
     wins.forEach(win => {
         if ((cellText[win[0]].innerText === cellText[win[1]].innerText) && (cellText[win[2]].innerText === cellText[win[1]].innerText) && (cellText[win[0]].innerText !== "")) {
             turnIndicator.innerHTML = `${cellText[win[0]].innerText} <span>Won</span>`;
 
-            isGameOver = true;
+            isGameOver = true;            
 
             gameMusic.play();
 
             gameOverImage[0].style.width = "200px";
+            document.querySelector(".gameover-line").style.width = `20vw`;
+            document.querySelector(".gameover-line").style.transform = `translate(${win[3]}vw, ${win[4]}vw) rotate(${win[5]}deg)`;
         }
     });
 };
@@ -50,6 +52,8 @@ Array.from(cells).forEach(element => {
     let cellText = element.querySelector(".cell-text");
 
     element.addEventListener("click", () => {
+        if (isGameOver) return;
+
         if (cellText.innerText === "") {
             cellText.innerText = turn;
         } else {
@@ -87,4 +91,5 @@ const reset = () => {
     gameMusic.currentTime = 0;
 
     gameOverImage[0].style.width = "0";
+    document.querySelector(".gameover-line").style.width = `0`;
 };
